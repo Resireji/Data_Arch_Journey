2c9022b7b8b4fe7d340fc3f07e0b9aede2863fb6
Resi Reji <resireji309@gmail.com>
2026-09-05 04:09:37 -0700

    Day 2:input, type casting, arithmetic

---
+++ day2.py
@@ -0,0 +1,6 @@
+#Data types, input(), and arithmetic
+name = input("what is your name?");
+birth_year = int(input("what is your birth year?"));
+age = 2026 - birth_year;
+year_turn_100 = birth_year + 100;
+print (f"Hi {name}, you are about {age} years old, and you will turn 100 in {year_turn_100}")
